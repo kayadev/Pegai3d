2e526cafff33b7a2f857ed9112e5852f824f9528
kayadev <m.mustafakaya@icloud.com>
2025-10-09 09:58:46 +0300

    Initial Commit

---
+++ Pegai3d/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pegai3d
+//
+//  Created by KAYA on 9.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pegai3d/Pegai3dApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pegai3dApp.swift
+//  Pegai3d
+//
+//  Created by KAYA on 9.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct Pegai3dApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
